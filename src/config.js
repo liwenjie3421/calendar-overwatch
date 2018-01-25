91716a7fa7dba16cff26dc11a837d459607fe5ec
liwenjie <liwenjie2@jk.pingan-inc.cn>
2018-01-25 18:31:05 +0800

    地址判断domain

--- src/config.js
+++ src/config.js
@@ -1,1 +1,8 @@
-export const domain = 'https://xn--2brq06crqr.xn--6qq986b3xl/api';
+export const domain = (() => {
+    let domain = 'http://localhost:7001';
+
+    if (window.location.href.indexOf('刘腊梅.我爱你') !== -1) {
+        domain = 'https://xn--2brq06crqr.xn--6qq986b3xl/api';
+    }
+    return domain;
+})();
